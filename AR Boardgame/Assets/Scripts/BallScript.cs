8226367ffa4c4d48aaf65f119964818a219f2f89
Maciej Wanat <maciejwanat@10g.pl>
2019-01-19 19:33:40 +0100

    Working version

---
+++ AR Boardgame/Assets/Scripts/BallScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _plane;
+
+    [SerializeField]
+    private GameObject _spawnPoint;
+
+    void FixedUpdate()
+    {
+        if (transform.position.y < _plane.transform.position.y - 10)
+        {
+            transform.position = _spawnPoint.transform.position;
+        }
+    }
+}
