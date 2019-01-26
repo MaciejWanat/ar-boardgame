9be20b81eb8a89d8df5e2126b320fd43e7551fb3
Maciej Wanat <maciejwanat@10g.pl>
2019-01-26 16:01:21 +0100

    Add some basic code

--- AR Boardgame/Assets/Scripts/GameManager.cs
+++ AR Boardgame/Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,21 +10,53 @@ public class GameManager : MonoBehaviour
     private Text _pointsText;
 
     [SerializeField]
+    private Text _timeText;
+
+    [SerializeField]
+    private Text _bestTimeText;
+
+    [SerializeField]
     private GameObject _resetButton;
 
     public GameObject UI;
 
     private int _points = 0;
+    private float _startTime;
+    private Tuple<string, int> _bestTime = new Tuple<string, int>("", int.MaxValue);
+    private bool _gameOn = false;
 
     void Start()
     {
         _pointsText.text = "";
+        _timeText.text = "";
+        _bestTimeText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        if (_gameOn)
+        {
+            float t = Time.time - _startTime;
+
+            var min = ((int)t / 60).ToString();
+            var sec = (t % 60).ToString("f2");
+
+            _timeText.text = "Time: " + min + ":" + sec;
+        }
     }
 
     public void AddPoints(int points)
     {
         _points += points;
         _pointsText.text = "Points: " + _points.ToString();
+
+        var time = Int32.Parse(_timeText.text.Replace(":", "").Replace(",", ""));
+        if (time < _bestTime.Item2)
+        {
+            _bestTime = new Tuple<string, int>(_timeText.text, time);
+        }
+
+        _bestTimeText.text = "Best: " + _bestTime.Item1;
     }
 
     public void TurnUserTargetUIOff()
@@ -31,6 +64,8 @@ public class GameManager : MonoBehaviour
         UI.SetActive(false);
         _pointsText.text = "Points: " + _points.ToString();
         _resetButton.SetActive(true);
+        _gameOn = true;
+        _startTime = Time.time;
     }
 
     public void ResetGame()
@@ -39,5 +74,7 @@ public class GameManager : MonoBehaviour
         _points = 0;
         _pointsText.text = "";
         _resetButton.SetActive(false);
+        _gameOn = false;
+        _timeText.text = "";
     }
 }
